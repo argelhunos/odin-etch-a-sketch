abd0ed4ffafbc80f47401f983f5ca44657db5847
argelhunos <52228971+argelhunos@users.noreply.github.com>
2023-12-29 23:35:15 -0500

    Add drawing capability and gamepad border

--- script.js
+++ script.js
@@ -4,6 +4,14 @@ for (let i = 0; i < 16*16; i++) {
     const tile = document.createElement('div');
     tile.style.width = "50px";
     tile.style.height = "50px";
+    tile.className = "tile";
+
+    tile.addEventListener('mouseover', function() {
+        if (tile.className != "tile painted") {
+            tile.className += " painted";
+        }
+    });
 
     gamePad.appendChild(tile);
 }
+
